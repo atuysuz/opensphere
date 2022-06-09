1f806c9a9f7a3cf30e5dbd6208a0dcfdc1be4170
Ahmet Tuysuzoglu <ahmet.tuysuzoglu@uber.com>
2022-06-09 17:36:50 -0400

    Updates to data copy file

--- copy_data.sh
+++ copy_data.sh
@@ -9,8 +9,8 @@ if [ ! -d "$BASE_PATH"/facerec_data ]; then
   mkdir -p DATA_PATH_VAL
   cp /mnt/share/deeplearning_uface-face-detection/datasets/vggface2/vggface2.tar DATA_PATH_TRAIN
   cp /mnt/share/deeplearning_uface-face-detection/datasets/lfw/lfw-deepfunneled.tgz DATA_PATH_VAL
-  tar xvf "$DATA_PATH"/vggface2.tar -C "$DATA_PATH_TRAIN"
-  tar xvf "$DATA_PATH"/lfw-deepfunneled.tgz -C "$DATA_PATH_VAL"
+  tar xvf "$DATA_PATH_TRAIN"/vggface2.tar -C "$DATA_PATH_TRAIN"
+  tar xvf "$DATA_PATH_VAL"/lfw-deepfunneled.tgz -C "$DATA_PATH_VAL"
   rm "$DATA_PATH_TRAIN"/vggface2.tar
   rm "$DATA_PATH_VAL"/lfw-deepfunneled.tgz
   cp /mnt/share/deeplearning_uface-face-detection/datasets/lfw/lfw_test_pair.txt "$DATA_PATH_VAL"
