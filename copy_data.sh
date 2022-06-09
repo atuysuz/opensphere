a566b9f196f49e8413682f7c2760849a01e3024b
Ahmet Tuysuzoglu <ahmet.tuysuzoglu@uber.com>
2022-06-09 17:20:54 -0400

    Updates to config

--- copy_data.sh
+++ copy_data.sh
@@ -3,13 +3,15 @@
 
 if [ ! -d "$BASE_PATH"/facerec_data ]; then
 
-  DATA_PATH="$BASE_PATH"/opensphere/data
-  mkdir "$DATA_PATH"
-  cp /mnt/share/deeplearning_uface-face-detection/datasets/vggface2/vggface2.tar "$DATA_PATH"
-  cp /mnt/share/deeplearning_uface-face-detection/datasets/lfw/lfw-deepfunneled.tgz "$DATA_PATH"
-  tar xvf "$DATA_PATH"/vggface2.tar -C "$DATA_PATH"
-  tar xvf "$DATA_PATH"/lfw-deepfunneled.tgz -C "$DATA_PATH"
-  rm "$DATA_PATH"/vggface2.tar
-  rm "$BASE_PATH"/facerec_data/lfw-deepfunneled.tgz
-  cp /mnt/share/deeplearning_uface-face-detection/datasets/lfw/lfw_test_pair.txt "$BASE_PATH"/facerec_data
+  DATA_PATH_TRAIN="$BASE_PATH"/opensphere/data/train
+  DATA_PATH_VAL="$BASE_PATH"/opensphere/data/val
+  mkdir -p DATA_PATH_TRAIN
+  mkdir -p DATA_PATH_VAL
+  cp /mnt/share/deeplearning_uface-face-detection/datasets/vggface2/vggface2.tar DATA_PATH_TRAIN
+  cp /mnt/share/deeplearning_uface-face-detection/datasets/lfw/lfw-deepfunneled.tgz DATA_PATH_VAL
+  tar xvf "$DATA_PATH"/vggface2.tar -C "$DATA_PATH_TRAIN"
+  tar xvf "$DATA_PATH"/lfw-deepfunneled.tgz -C "$DATA_PATH_VAL"
+  rm "$DATA_PATH_TRAIN"/vggface2.tar
+  rm "$DATA_PATH_VAL"/lfw-deepfunneled.tgz
+  cp /mnt/share/deeplearning_uface-face-detection/datasets/lfw/lfw_test_pair.txt "$DATA_PATH_VAL"
 fi
